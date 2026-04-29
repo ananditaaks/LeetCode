9c14fdc32f25bab91ba4a7af193d47842022e36b
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-04-29 13:40:56 +0530

    Day 1:Majority Element

---
+++ src/top150/MajorityElement.java
@@ -0,0 +1,24 @@
+package top150;
+
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int candidate = 0, count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        MajorityElement obj = new MajorityElement();
+        int[] nums = {2,2,1,1,1,2,2};
+
+        System.out.println(obj.majorityElement(nums));
+    }
+}
