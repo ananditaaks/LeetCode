6bda8aa5bd1d63735f39175e71e28bddab9984fe
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-05-01 20:30:28 +0530

    Day 3:Product Except Self

---
+++ src/top150/ProductExceptSelf.java
@@ -0,0 +1,30 @@
+package top150;
+
+public class ProductExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ProductExceptSelf obj = new ProductExceptSelf();
+        int[] nums = {1,2,3,4};
+
+        int[] res = obj.productExceptSelf(nums);
+        for (int x : res) System.out.print(x + " ");
+    }
+}
