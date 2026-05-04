378dce81ef0e4ab30f188524a102ad46e2abe75a
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-05-04 18:02:47 +0530

    Day 6: Group Anagrams

---
+++ src/top150/GroupAnagrams.java
@@ -0,0 +1,27 @@
+package top150;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams obj = new GroupAnagrams();
+        String[] input = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(obj.groupAnagrams(input));
+    }
+}
