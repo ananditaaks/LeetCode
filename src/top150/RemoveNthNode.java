3dc1c419046d52b4c6dcc6ccc1e6d22dffb24e71
Ananditakant Ojha <ananditakantojha@gmail.com>
2026-05-11 21:39:12 +0530

    Day 13: Remove Nth Node

---
+++ src/top150/RemoveNthNode.java
@@ -0,0 +1,35 @@
+package top150;
+
+public class RemoveNthNode {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
